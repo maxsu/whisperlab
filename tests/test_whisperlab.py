59fa667062e9b583732b72df512a7c0750127b72
Max Suica <max.suica@gmail.com>
2023-11-27 18:26:21 -0800

    feat(tests): Add tests module stub

---
+++ tests/test_whisperlab.py
@@ -0,0 +1,16 @@
+import whisperlab
+
+def test_whisper():
+    assert(whisperlab is not None)
+
+def test_verbose_mode():
+    assert(False)
+
+def test_structured_mode():
+    assert(False)
+
+def test_transcribe():
+    assert(False)
+
+def test_transcribe_profile():
+    assert(False)
