632d5beb22cf3aac45c747de12b1198e23ab8ec3
Max Suica <max.suica@gmail.com>
2023-12-01 11:11:32 -0800

    feat(test_whisperlab) Add a new test poem - sappho 58
    * Test poem transcription

--- tests/test_whisperlab.py
+++ tests/test_whisperlab.py
@@ -8,3 +8,15 @@ config_log()
 
 def test_whisper():
     assert whisperlab is not None
+
+
+def test_transcribe():
+    request = whisperlab.WhisperRequest(
+        audio_file="tests/data/poem_sappho_58_by_Jameson_Fitzpatrick.mp3",
+    )
+
+    result = whisperlab.run_whisper(request)
+
+    assert result["text"] is not None
+
+    log.info(result["text"])
