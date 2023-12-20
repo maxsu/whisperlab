897aadb20bf7b14787737677b4756ef4a7d448dc
Max Suica <Max.Suica@gmail.com>
2023-12-19 18:23:07 -0800

    Simplify microphone app

--- src/whisperlab/microphone.py
+++ src/whisperlab/microphone.py
@@ -4,7 +4,6 @@
 Matplotlib and NumPy have to be installed.
 
 """
-import argparse
 import queue
 import sys
 
@@ -22,64 +21,15 @@ def int_or_str(text):
         return text
 
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    "-l",
-    "--list-devices",
-    action="store_true",
-    help="show list of audio devices and exit",
-)
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser],
-)
-parser.add_argument(
-    "channels",
-    type=int,
-    default=[1],
-    nargs="*",
-    metavar="CHANNEL",
-    help="input channels to plot (default: the first)",
-)
-parser.add_argument(
-    "-d", "--device", type=int_or_str, help="input device (numeric ID or substring)"
-)
-parser.add_argument(
-    "-w",
-    "--window",
-    type=float,
-    default=200,
-    metavar="DURATION",
-    help="visible time slot (default: %(default)s ms)",
-)
-parser.add_argument(
-    "-i",
-    "--interval",
-    type=float,
-    default=30,
-    help="minimum time between plot updates (default: %(default)s ms)",
-)
-parser.add_argument("-b", "--blocksize", type=int, help="block size (in samples)")
-parser.add_argument(
-    "-r", "--samplerate", type=float, help="sampling rate of audio device"
-)
-parser.add_argument(
-    "-n",
-    "--downsample",
-    type=int,
-    default=10,
-    metavar="N",
-    help="display every Nth sample (default: %(default)s)",
-)
-args = parser.parse_args(remaining)
-if any(c < 1 for c in args.channels):
-    parser.error("argument CHANNEL: must be >= 1")
-mapping = [c - 1 for c in args.channels]  # Channel numbers start with 1
+CHANNEL = 1  # Input channel to use (assume 1 microphone channel)
+DEVICE = None  # Input device (use default device)
+SAMPLERATE = None  # Sample rate (in samples/sec) (use default)
+BLOCKSIZE = None  # Block size (in samples) (use default)
+DOWNSAMPLE = 10  # Display every Nth sample
+INTERVAL = 30  # Minimum time between plot updates (in ms)
+WINDOW = 200  # Width of plot window (in ms)
+
+mapping = [0]
 q = queue.Queue()
 
 
@@ -88,7 +38,7 @@ def audio_callback(indata, frames, time, status):
     if status:
         print(status, file=sys.stderr)
     # Fancy indexing with mapping creates a (necessary!) copy:
-    q.put(indata[:: args.downsample, mapping])
+    q.put(indata[::DOWNSAMPLE, mapping])
 
 
 def update_plot(frame):
@@ -112,22 +62,14 @@ def update_plot(frame):
     return lines
 
 
-try:
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, "input")
-        args.samplerate = device_info["default_samplerate"]
+if __name__ == "__main__":
+    SAMPLERATE = sd.query_devices(kind="input")["default_samplerate"]
 
-    length = int(args.window * args.samplerate / (1000 * args.downsample))
-    plotdata = np.zeros((length, len(args.channels)))
+    length = int(WINDOW * SAMPLERATE / (1000 * DOWNSAMPLE))
+    plotdata = np.zeros((length, 1))
 
     fig, ax = plt.subplots()
     lines = ax.plot(plotdata)
-    if len(args.channels) > 1:
-        ax.legend(
-            [f"channel {c}" for c in args.channels],
-            loc="lower left",
-            ncol=len(args.channels),
-        )
     ax.axis((0, len(plotdata), -1, 1))
     ax.set_yticks([0])
     ax.yaxis.grid(True)
@@ -142,13 +84,13 @@ try:
     fig.tight_layout(pad=0)
 
     stream = sd.InputStream(
-        device=args.device,
-        channels=max(args.channels),
-        samplerate=args.samplerate,
+        device=DEVICE,
+        channels=CHANNEL,
+        samplerate=SAMPLERATE,
         callback=audio_callback,
     )
-    ani = FuncAnimation(fig, update_plot, interval=args.interval, blit=True)
+    ani = FuncAnimation(fig, update_plot, interval=INTERVAL, blit=True)
     with stream:
         plt.show()
-except Exception as e:
-    parser.exit(type(e).__name__ + ": " + str(e))
+    while True:
+        pass
