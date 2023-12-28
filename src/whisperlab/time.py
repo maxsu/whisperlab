3e833bb5c98b41ade31c35c05ca8918135eaac85
Max Suica <Max.Suica@gmail.com>
2023-12-28 13:58:56 -0800

    feat(time): Add timestamp function

--- src/whisperlab/time.py
+++ src/whisperlab/time.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 
@@ -9,3 +10,10 @@ def time_ms():
         int: The current time in milliseconds since epoch.
     """
     return int(round(time.time() * 1000))
+
+
+def timestamp():
+    """
+    Get a formatted timestamp.
+    """
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
