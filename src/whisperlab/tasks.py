ef4a2428f89a29ba34f696839d972ccd125023f1
Max Suica <Max.Suica@gmail.com>
2023-12-28 13:53:54 -0800

    feat(tasks): Add completion status and timing
    Add completion status and timing to tasks.
    
    New fields:
    - completed
    - created_time
    - completed_time
    - duration
    - result
    
    New methods:
    -complete
    
    Updates:
    - change id from a uuid string to a raw UUID object

--- src/whisperlab/tasks.py
+++ src/whisperlab/tasks.py
@@ -1,8 +1,26 @@
+from typing import Optional, UUID
 from uuid import uuid4
+from pydantic import BaseModel, Field
 
-from pydantic import BaseModel
+from whisperlab.time import time_ms
 
 
 class Task(BaseModel):
-    id: str = uuid4()
+    # Task identification
+    id: UUID = Field(default_factory=uuid4)
+    batch: str = ""
+    sequence: int = 0
+
+    # Task status
+    completed: bool = False
     result: dict = {}
+
+    # Task timing
+    created_time: float = Field(default_factory=time_ms)
+    completed_time: float = None
+
+    def complete(self, result: dict):
+        """Complete the task with the given result."""
+        self.completed = True
+        self.result = result
+        self.completed_time = time_ms()
